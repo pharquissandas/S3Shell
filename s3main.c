dae001e4b3b1bcf0b2ff6631663a9c28349ba129
minimish1 <minimish007@gmail.com>
2025-11-16 21:47:14 +0000

    added batched commands
    Added support for batch command execution in the command line interface.

--- s3main.c
+++ s3main.c
@@ -11,22 +11,53 @@ int main(int argc, char *argv[]) {
     while (1) {
         read_command_line(line, lwd);
 
-        if (is_cd(line)) {
-            parse_command(line, args, &argsc);
-            run_cd(args, argsc, lwd);
-        } else if (command_with_pipe(line)) {
-            char *commands[MAX_ARGS];
-            int num_cmds = split_pipeline(line, commands);
-            launch_pipeline(commands, num_cmds);
-            reap();
-        } else if (command_with_redirection(line)) {
-            parse_command(line, args, &argsc);
-            launch_program_with_redirection(args, argsc);
-            reap();
+        // repeat for each batched command
+        if (command_with_batch(line)) {
+            char *batch[MAX_ARGS];
+            int batch_count = split_batch(line, batch);
+
+            for (int i = 0; i < batch_count; i++) {
+                char *cmd = batch[i];
+
+                if (is_cd(cmd)) {
+                    parse_command(cmd, args, &argsc);
+                    run_cd(args, argsc, lwd);
+                } else if (command_with_pipe(cmd)) {
+                    char *pipe_commands[MAX_ARGS];
+                    int num_cmds = split_pipeline(cmd, pipe_commands);
+                    launch_pipeline(pipe_commands, num_cmds);
+                    reap();
+                } else if (command_with_redirection(cmd)) {
+                    parse_command(cmd, args, &argsc);
+                    launch_program_with_redirection(args, argsc);
+                    reap();
+                } else {
+                    parse_command(cmd, args, &argsc);
+                    launch_program(args, argsc);
+                    reap();
+                }
+            }
+
         } else {
-            parse_command(line, args, &argsc);
-            launch_program(args, argsc);
-            reap();
+
+            // non batched handling
+            if (is_cd(line)) {
+                parse_command(line, args, &argsc);
+                run_cd(args, argsc, lwd);
+            } else if (command_with_pipe(line)) {
+                char *commands[MAX_ARGS];
+                int num_cmds = split_pipeline(line, commands);
+                launch_pipeline(commands, num_cmds);
+                reap();
+            } else if (command_with_redirection(line)) {
+                parse_command(line, args, &argsc);
+                launch_program_with_redirection(args, argsc);
+                reap();
+            } else {
+                parse_command(line, args, &argsc);
+                launch_program(args, argsc);
+                reap();
+            }
         }
     }
 
